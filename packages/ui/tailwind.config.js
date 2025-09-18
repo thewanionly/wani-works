1026baf6fcbf1b855799562f5e5316986704f5b6
thewanionly <pelloani@gmail.com>
2025-09-19 02:14:02 +0800

    chore: put the sharedConfig inline

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,7 +1,14 @@
-import sharedConfig from '@wani-works/tailwind-config';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{ts,tsx}'],
-  presets: [sharedConfig],
+  theme: {
+    extend: {
+      boxShadow: {
+        buttonFocusRing: '0px 0px 0px 4px rgb(68 76 231 / 0.12)',
+        destructiveBtnFocusRing: '0px 0px 0px 4px rgb(217 45 32 / 0.12)',
+      },
+    },
+  },
+  plugins: [],
+  presets: [],
 };
