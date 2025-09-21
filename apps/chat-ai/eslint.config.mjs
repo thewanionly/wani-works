0ac2c8d65fe6be4fde7710ca155b41d7c2d082f1
thewanionly <pelloani@gmail.com>
2025-09-22 03:01:00 +0800

    chore: set up eslint

--- apps/chat-ai/eslint.config.mjs
+++ apps/chat-ai/eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 import { FlatCompat } from '@eslint/eslintrc';
+import { nextJsConfig } from '@wani-works/eslint-config/next-js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  ...nextJsConfig,
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: [
