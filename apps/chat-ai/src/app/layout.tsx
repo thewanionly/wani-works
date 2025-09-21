0a9d13ffeee5be494426b54b627d1c740e260ba2
thewanionly <pelloani@gmail.com>
2025-09-22 05:07:21 +0800

    refactor: use native tailwind util classes for styling

--- apps/chat-ai/src/app/layout.tsx
+++ apps/chat-ai/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${notoSans.variable} font-primary bg-background text-foreground antialiased`}
+        className={`${notoSans.variable} font-primary bg-white text-neutral-900 antialiased`}
       >
         {children}
       </body>
